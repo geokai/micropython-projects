c1bf1361c31f936df569dfdc0636b36c42ecbef0
George Kaimakis <geomatus@mac.com>
2017-03-13 07:50:54 +0000

    Interval change. 2 second delay placed after measure:

--- temp_hum.py
+++ temp_hum.py
@@ -7,11 +7,11 @@ sen = dht.DHT22(machine.Pin(2))
 counter = 0
 while True:
     print("Checking Temp & Humidity...{0}".format(counter + 1))
-    sleep(2)
     #print()
     sen.measure()
+    sleep(2)
     print("Temp \t{0:3.1f}C".format(sen.temperature()))
     print("Hum \t{0:3.1f}%".format(sen.humidity()))
     print()
     counter += 1
-    sleep(180)
+    sleep(300)
