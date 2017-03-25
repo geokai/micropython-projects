3cc54406627cd9112fd99fd19e475d6803614cd9
George Kaimakis <geomatus@mac.com>
2017-03-25 23:56:27 +0000

    temp_hum.py renamed - specified for dht sensor:

---
+++ temp_hum_dht.py
@@ -0,0 +1,17 @@
+import machine
+import dht
+from time import sleep
+
+sen = dht.DHT22(machine.Pin(2))
+
+counter = 0
+while True:
+    print("Checking Temp & Humidity...{0}".format(counter + 1))
+    #print()
+    sen.measure()
+    sleep(0.5)
+    print("Temperature \t{0:3.1f} Celcius".format(sen.temperature()))
+    print("Humidity \t{0:3.1f} % RH".format(sen.humidity()))
+    print()
+    counter += 1
+    sleep(600)
