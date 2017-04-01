42452a9f0664180a664e518227b47f250f6c4e02
George Kaimakis <geomatus@mac.com>
2017-04-02 00:34:50 +0100

    Adds led indicator - brief flash when reading is taken:

--- temp_hum_bme.py
+++ temp_hum_bme.py
@@ -1,4 +1,4 @@
-# This script uses the DHT22 sensor to return temperature and humidity.
+# This script uses the BME280 sensor to return temperature and humidity.
 # Uses a one-wire method of connection to the device:
 
 # This file was created on 30/03/2017
@@ -9,13 +9,16 @@ import machine
 import bme280
 from time import sleep
 
-
+PIXEL_PIN   = 16    # used as 'reading sensor' indicator:
 INTERVAL    = 10    # delay between readings:
 PAUSE       = 0.5   # small pause before printing results (for effect!):
+BLIP        = 0.05  # tiny delay for the led indicator:
 
 # create objects for i2c and the bme280 sensor:
 i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
 bme = bme280.BME280(i2c=i2c)
+
+led = machine.Pin(PIXEL_PIN, machine.Pin.OUT)
 
 # print the device id before entering the loop:
 print()
@@ -28,6 +31,9 @@ while True:
     print("Checking Temp, Pressure & Humidity ...{0}".format(counter + 1))
     #print()
     readings = bme.read_compensated_data()
+    led.low()
+    sleep(BLIP)
+    led.high()
     sleep(PAUSE)
     print("Temperature  \t{0:6.1f} Celcius".format(readings[0]/100))
     print("Pressure \t{0:6.1f} hPa".format(readings[1]/256/100))
