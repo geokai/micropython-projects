a9508ffa9a8741582851edc212dcb354acafebe4
George Kaimakis <geomatus@mac.com>
2017-04-17 19:17:08 +0100

    Corrections to 'readings' out of scope issue:
    Corrected the major blunder of pulling the sensor 'reading' call out of the loop.
    That was part of the attempt to eliminate the 'magic numbers' factors
    required to correct the readings.
    All have now been restored with additional exlanatory comments.

--- temp_hum_bme.py
+++ temp_hum_bme.py
@@ -14,7 +14,7 @@ import machine
 import bme280
 from time import sleep
 
-PIXEL_PIN   = 16    # used as 'reading sensor' indicator:
+BOARD_LED   = 16    # on-board led used as 'reading sensor' indicator:
 INTERVAL    = 10    # interval between readings:
 PAUSE       = 0.5   # small pause before printing results (for effect!):
 BLIP        = 0.05  # tiny delay for the led indicator:
@@ -24,7 +24,8 @@ BLIP        = 0.05  # tiny delay for the led indicator:
 i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
 bme = bme280.BME280(i2c=i2c)
 
-led = machine.Pin(PIXEL_PIN, machine.Pin.OUT)
+# create led indicator object:
+led = machine.Pin(BOARD_LED, machine.Pin.OUT)
 
 # print the device id before entering the loop:
 print()
@@ -32,32 +33,30 @@ print('i2c devices found: {0}'.format(i2c.scan()))
 print()
 
 
+# momentary led flash (blip!) to indicate sensor reading:
 def led_flash():
     led.low()
     sleep(BLIP)
     led.high()
 
 
-# aquire readings from bme280 sensor:
-readings = bme.read_compensated_data()
-
-# Adjustment factors for aquired readings (refer to bme280 datasheet):
-temp_factor     = readings[0]/100       # temerature:
-press_factor    = readings[1]/256/100   # barometric pressure:
-humi_factor     = readings[2]/1024      # relative humidity:
-
 counter = 0     # reset counter:
 
 # main loop:
 while True:
-    print("Checking...", end='')
-    sleep(PAUSE)
-    print("\rTemperature, Pressure & Humidity ...{0}".format(counter + 1))
+    print("Checking", end='')
     led_flash()
+    readings = bme.read_compensated_data()
     sleep(PAUSE)
-    print("Temp:  \t{0:6.1f} Celcius".format(temp_factor))
-    print("Pres: \t{0:6.1f} hPa/mb".format(press_factor))
-    print("Humi: \t{0:6.1f} % RH".format(humi_factor))
+    for i in range(3):
+        print('.', end='')
+        sleep(PAUSE)
+    print("\rTemperature, Pressure & Humidity ...{0}".format(counter + 1))
+    sleep(PAUSE)
+    # Adjustment factors for aquired readings (refer to bme280 datasheet):
+    print("Temp:  \t{0:6.1f} Celcius".format(readings[0]/100))
+    print("Pres: \t{0:6.1f} hPa/mb".format(readings[1]/256/100))
+    print("Humi: \t{0:6.1f} % RH".format(readings[2]/1024))
     print()
-    counter += 1    # increment counter:
-    sleep(INTERVAL)
+    counter += 1        # increment counter:
+    sleep(INTERVAL)     # loop cycle:
